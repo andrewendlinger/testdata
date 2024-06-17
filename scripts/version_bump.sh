d3bb56c82aede2bb55b8a1dc8b503ab5fbba60c5
Andrecho <37243793+andrewendlinger@users.noreply.github.com>
2024-06-17 12:14:38 +0200

    outsourced script from github actions

---
+++ scripts/version_bump.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Ensure the script exits on the first error encountered
+set -e
+
+# Check if CHANGELOG.md exists; create it if it doesn't
+if [ ! -f CHANGELOG.md ]; then
+    touch CHANGELOG.md
+fi
+
+# Fetch all tags and latest changes
+git fetch --tags
+git pull origin main
+
+# Fetch latest tag and date
+latest_tag=$(git describe --tags --abbrev=0)
+latest_tag_date=$(git log -1 --format=%ai $latest_tag | cut -d ' ' -f 1)
+formatted_date=$(date -d $latest_tag_date +"%dth of %B %Y")
+
+# Generate changelog header
+echo "## $latest_tag ($formatted_date)" > CHANGELOG.md
+
+# Append commits to changelog
+git log --pretty=format:"- [%h](https://github.com/andrewendlinger/testdata/commit/%H) %s" $latest_tag..HEAD >> CHANGELOG.md
+
+# Add a new line at the end of the file
+echo "" >> CHANGELOG.md
+
+# Commit changelog
+git add CHANGELOG.md
+git commit -m "Updated changelog from $latest_tag to HEAD"
+git push origin HEAD:main
