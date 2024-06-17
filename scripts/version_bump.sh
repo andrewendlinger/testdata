0871740a266343387c1325cd359732c455048089
Andrecho <37243793+andrewendlinger@users.noreply.github.com>
2024-06-17 15:01:19 +0200

    xkf

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -33,7 +33,7 @@ echo "## $current_version (date)" >> $temp_file
 
 # Loop through all commits since the last push and append to the temporary file
 echo "Changes since last push to main:" >> "$temp_file"
-echo git log --pretty=format:"- [%h](https://github.com/andrewendlinger/testdata/commit/%H) %s" "v$current_version..HEAD"
+git log --pretty=format:"- [%h](https://github.com/andrewendlinger/testdata/commit/%H) %s" "v$current_version..HEAD"
 git log --pretty=format:"- [%h](https://github.com/andrewendlinger/testdata/commit/%H) %s" "v$current_version..HEAD" >> "$temp_file"
 
 # Add a new line at the end of the temporary file
@@ -55,5 +55,6 @@ git push origin "v$new_version"
 
 # Commit version bump
 git add version.txt
+git add CHANGELOG.md
 git commit -m "Bump version to $new_version"
 git push origin HEAD:main
